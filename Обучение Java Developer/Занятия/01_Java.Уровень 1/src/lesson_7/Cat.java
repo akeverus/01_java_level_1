368f2d59e59001a14a0d264084780f5d86f056de
voronins <polaroids.of.tarkovsky@gmail.com>
2021-07-07 13:45:06 +0300

    Сделал ДЗ за 06.07.2021

--- Обучение Java Developer/Занятия/01_Java.Уровень 1/src/lesson_7/Cat.java
+++ Обучение Java Developer/Занятия/01_Java.Уровень 1/src/lesson_7/Cat.java
@@ -1,14 +1,48 @@
 package lesson_7;
 
 public class Cat {
-    private String name;
-    private int appetite;
-    public Cat(String name, int appetite) {
+    private final String name;
+    private final int appetite;
+    private boolean satiety;
+
+    public Cat(String name, int appetite, boolean satiety) {
         this.name = name;
         this.appetite = appetite;
+        this.satiety = satiety;
     }
-    public void eat(Plate p) {
-        p.decreaseFood(appetite);
+
+    public boolean isSatiety() {
+        return satiety;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSatiety(boolean satiety) {
+        this.satiety = satiety;
+    }
+
+
+    public boolean eat(Plate p, boolean satiety) {
+        if (!satiety) {
+            if (p.getFood() >= appetite) {
+                p.decreaseFood(appetite);
+                satiety = true;
+            }
+        } else {
+            System.out.println("Кот уже сыт и не будет есть из миски");
+        }
+        return satiety;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", appetite=" + appetite +
+                ", satiety=" + satiety +
+                '}';
     }
 }
 
